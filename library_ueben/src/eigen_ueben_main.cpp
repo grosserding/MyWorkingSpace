64cfe11551663038d73b47d4a258432f1dc41996
DasDickeDing <dingzhicong_auto@163.com>
2024-03-30 23:08:25 +0800

    can build and run

--- library_ueben/src/eigen_ueben_main.cpp
+++ library_ueben/src/eigen_ueben_main.cpp
@@ -1,5 +1,8 @@
+#include "lib/eigen_ueben_lib.h"
 #include <iostream>
-int main(int argc, char** argv) {
-  std::cout << "library ueben!" << std::endl;
+int main(int argc, char **argv)
+
+{
+  HelloWorld::PrintHelloWorld();
   return 0;
 }
--- library_ueben/src/lib/eigen_ueben_lib.cpp
+++ library_ueben/src/lib/eigen_ueben_lib.cpp
@@ -1,1 +1,10 @@
-#include <iostream>
+#include "lib/eigen_ueben_lib.h"
+
+namespace HelloWorld
+{
+    void PrintHelloWorld()
+    {
+        std::cout << "library ueben!" << std::endl;
+        std::cerr << "new line in new Ubuntu 22.04" << std::endl;
+    }
+}
