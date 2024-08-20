58b7658fe4ec8715166eeb78952c31f0a40afa57
zhicong.ding(at_office) <dingzhicong_auto@163.com>
2024-08-20 18:09:07 +0800

    g2o cmake fails

---
+++ SlamUeben/src/vierzehn_kapiteln_des_SLAMs/kapitel_6/6.4.cpp
@@ -0,0 +1,15 @@
+#include <g2o/core/base_unary_edge.h>
+#include <g2o/core/base_vertex.h>
+#include <g2o/core/block_solver.h>
+#include <g2o/core/optimization_algorithm_dogleg.h>
+#include <g2o/core/optimization_algorithm_gauss_newton.h>
+#include <g2o/core/optimization_algorithm_levenberg.h>
+#include <g2o/solvers/dense/linear_solver_dense.h>
+
+#include <Eigen/Core>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <opencv2/core/core.hpp>
+
+int main(int argc, char** argv) { return 0; }
