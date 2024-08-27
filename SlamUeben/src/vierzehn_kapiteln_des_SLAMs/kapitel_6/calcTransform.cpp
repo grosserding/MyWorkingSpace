aed8880d0415f2e4752bd785ab600d6422e5e3c0
zhicong.ding(at_office) <dingzhicong_auto@163.com>
2024-08-27 18:24:06 +0800

    got good result now

--- SlamUeben/src/vierzehn_kapiteln_des_SLAMs/kapitel_6/calcTransform.cpp
+++ SlamUeben/src/vierzehn_kapiteln_des_SLAMs/kapitel_6/calcTransform.cpp
@@ -211,42 +211,14 @@ Point2d pixel2cam(const Point2d& p, const Mat& K);
 void pose_estimation_3d3d(const vector<Point3f>& pts1,
                           const vector<Point3f>& pts2, Mat& R, Mat& t);
 int main(int argc, char** argv) {
-  if (argc != 5) {
-    cout << "usage: pose_estimation_3d3d img1 img2 depth1 depth2" << endl;
-    return 1;
-  }
-  double camera[6] = {0, 1, 2, 0, 0, 0};
-  //-- 读取图像
-  Mat img_1 = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-  Mat img_2 = imread(argv[2], CV_LOAD_IMAGE_COLOR);
-
-  vector<KeyPoint> keypoints_1, keypoints_2;
-  vector<DMatch> matches;
-  find_feature_matches(img_1, img_2, keypoints_1, keypoints_2, matches);
-  cout << "一共找到了" << matches.size() << "组匹配点" << endl;
-
-  // 建立3D点
-  Mat depth1 = imread(
-      argv[3], CV_LOAD_IMAGE_UNCHANGED);  // 深度图为16位无符号数，单通道图像
-  Mat depth2 = imread(
-      argv[4], CV_LOAD_IMAGE_UNCHANGED);  // 深度图为16位无符号数，单通道图像
-  Mat K = (Mat_<double>(3, 3) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1);
   vector<Point3f> pts1, pts2;
-
-  for (DMatch m : matches) {
-    ushort d1 = depth1.ptr<unsigned short>(
-        int(keypoints_1[m.queryIdx].pt.y))[int(keypoints_1[m.queryIdx].pt.x)];
-    ushort d2 = depth2.ptr<unsigned short>(
-        int(keypoints_2[m.trainIdx].pt.y))[int(keypoints_2[m.trainIdx].pt.x)];
-    if (d1 == 0 || d2 == 0)  // bad depth
-      continue;
-    Point2d p1 = pixel2cam(keypoints_1[m.queryIdx].pt, K);
-    Point2d p2 = pixel2cam(keypoints_2[m.trainIdx].pt, K);
-    float dd1 = float(d1) / 5000.0;
-    float dd2 = float(d2) / 5000.0;
-    pts1.push_back(Point3f(p1.x * dd1, p1.y * dd1, dd1));
-    pts2.push_back(Point3f(p2.x * dd2, p2.y * dd2, dd2));
-  }
+  double camera[6] = {0, 1, 2, 0, 0, 0};
+  pts1.push_back(Point3f(-107.339447,368.235229,7.203767));
+  pts1.push_back(Point3f(-13.938293,-3.508911,18.397972));
+  pts1.push_back(Point3f(36.682861,444.794983,-2.003768));
+  pts2.push_back(Point3f(-106.496429,369.124573,6.662053));
+  pts2.push_back(Point3f(0.446471,0.734270,-1.074847));
+  pts2.push_back(Point3f(35.070869,450.609863,7.601254));
 
   cout << "3d-3d pairs: " << pts1.size() << endl;
   Mat R, t;
