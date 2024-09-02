611b2f2e771a63045d49456b3d4236634251e1a0
zhicong.ding(at_office) <dingzhicong_auto@163.com>
2024-09-02 17:22:18 +0800

    format

--- SlamUeben/include/icp_omp.h
+++ SlamUeben/include/icp_omp.h
@@ -1,90 +1,106 @@
 #ifndef PCL_ICP_OMP_H_
 #define PCL_ICP_OMP_H_
- 
+
 // PCL includes
-#include <pcl/sample_consensus/ransac.h>
-#include <pcl/sample_consensus/sac_model_registration.h>
-#include <pcl/registration/registration.h>
-#include <pcl/registration/transformation_estimation_2D.h>
 #include <pcl/registration/default_convergence_criteria.h>
 #include <pcl/registration/icp.h>
+#include <pcl/registration/registration.h>
+#include <pcl/registration/transformation_estimation_2D.h>
+#include <pcl/sample_consensus/ransac.h>
+#include <pcl/sample_consensus/sac_model_registration.h>
+
 #include "correspondence_estimation_omp.h"
-namespace pcl
-{
-  /** \brief @b IterativeClosestPoint provides a base implementation of the Iterative Closest Point algorithm. 
-    * The transformation is estimated based on Singular Value Decomposition (SVD).
-    *
-    * The algorithm has several termination criteria:
-    *
-    * <ol>
-    * <li>Number of iterations has reached the maximum user imposed number of iterations (via \ref setMaximumIterations)</li>
-    * <li>The epsilon (difference) between the previous transformation and the current estimated transformation is smaller than an user imposed value (via \ref setTransformationEpsilon)</li>
-    * <li>The sum of Euclidean squared errors is smaller than a user defined threshold (via \ref setEuclideanFitnessEpsilon)</li>
-    * </ol>
-    *
-    *
-    * Usage example:
-    * \code
-    * IterativeClosestPoint<PointXYZ, PointXYZ> icp;
-    * // Set the input source and target
-    * icp.setInputCloud (cloud_source);
-    * icp.setInputTarget (cloud_target);
-    *
-    * // Set the max correspondence distance to 5cm (e.g., correspondences with higher distances will be ignored)
-    * icp.setMaxCorrespondenceDistance (0.05);
-    * // Set the maximum number of iterations (criterion 1)
-    * icp.setMaximumIterations (50);
-    * // Set the transformation epsilon (criterion 2)
-    * icp.setTransformationEpsilon (1e-8);
-    * // Set the euclidean distance difference epsilon (criterion 3)
-    * icp.setEuclideanFitnessEpsilon (1);
-    *
-    * // Perform the alignment
-    * icp.align (cloud_source_registered);
-    *
-    * // Obtain the transformation that aligned cloud_source to cloud_source_registered
-    * Eigen::Matrix4f transformation = icp.getFinalTransformation ();
-    * \endcode
-    *
-    * \author Radu B. Rusu, Michael Dixon
-    * \ingroup registration
-    */
-  template <typename PointSource, typename PointTarget, typename Scalar = float>
-  class IterativeClosestPointOMP : public IterativeClosestPoint<PointSource, PointTarget, Scalar>
-  {
-  public:
-      typedef typename Registration<PointSource, PointTarget, Scalar>::PointCloudSource PointCloudSource;
-      typedef typename PointCloudSource::Ptr PointCloudSourcePtr;
-      typedef typename PointCloudSource::ConstPtr PointCloudSourceConstPtr;
- 
-      typedef typename Registration<PointSource, PointTarget, Scalar>::PointCloudTarget PointCloudTarget;
-      typedef typename PointCloudTarget::Ptr PointCloudTargetPtr;
-      typedef typename PointCloudTarget::ConstPtr PointCloudTargetConstPtr;
- 
-      typedef PointIndices::Ptr PointIndicesPtr;
-      typedef PointIndices::ConstPtr PointIndicesConstPtr;
- 
-      typedef boost::shared_ptr<IterativeClosestPointOMP<PointSource, PointTarget, Scalar> > Ptr;
-      typedef boost::shared_ptr<const IterativeClosestPointOMP<PointSource, PointTarget, Scalar> > ConstPtr;
-      /** \brief Empty constructor. */
-      IterativeClosestPointOMP()
-          : IterativeClosestPoint<PointSource, PointTarget, Scalar>()
-      {
-          reg_name_ = "IterativeClosestPointOMP";
-          transformation_estimation_.reset(new pcl::registration::TransformationEstimationSVD<PointSource, PointTarget, Scalar>());
-          correspondence_estimation_.reset(new pcl::registration::CorrespondenceEstimationOMP<PointSource, PointTarget, Scalar>);
-          convergence_criteria_.reset(new pcl::registration::DefaultConvergenceCriteria<Scalar>(nr_iterations_, transformation_, *correspondences_));
-      };
- 
-      /** \brief Empty destructor */
-      virtual ~IterativeClosestPointOMP() {}
- 
-      double getFitnessScore(const PointCloudSourceConstPtr& cloud,
-          double max_range = std::numeric_limits<double>::max());
- 
+namespace pcl {
+/** \brief @b IterativeClosestPoint provides a base implementation of the
+ * Iterative Closest Point algorithm. The transformation is estimated based on
+ * Singular Value Decomposition (SVD).
+ *
+ * The algorithm has several termination criteria:
+ *
+ * <ol>
+ * <li>Number of iterations has reached the maximum user imposed number of
+ * iterations (via \ref setMaximumIterations)</li> <li>The epsilon (difference)
+ * between the previous transformation and the current estimated transformation
+ * is smaller than an user imposed value (via \ref
+ * setTransformationEpsilon)</li> <li>The sum of Euclidean squared errors is
+ * smaller than a user defined threshold (via \ref
+ * setEuclideanFitnessEpsilon)</li>
+ * </ol>
+ *
+ *
+ * Usage example:
+ * \code
+ * IterativeClosestPoint<PointXYZ, PointXYZ> icp;
+ * // Set the input source and target
+ * icp.setInputCloud (cloud_source);
+ * icp.setInputTarget (cloud_target);
+ *
+ * // Set the max correspondence distance to 5cm (e.g., correspondences with
+ * higher distances will be ignored) icp.setMaxCorrespondenceDistance (0.05);
+ * // Set the maximum number of iterations (criterion 1)
+ * icp.setMaximumIterations (50);
+ * // Set the transformation epsilon (criterion 2)
+ * icp.setTransformationEpsilon (1e-8);
+ * // Set the euclidean distance difference epsilon (criterion 3)
+ * icp.setEuclideanFitnessEpsilon (1);
+ *
+ * // Perform the alignment
+ * icp.align (cloud_source_registered);
+ *
+ * // Obtain the transformation that aligned cloud_source to
+ * cloud_source_registered Eigen::Matrix4f transformation =
+ * icp.getFinalTransformation (); \endcode
+ *
+ * \author Radu B. Rusu, Michael Dixon
+ * \ingroup registration
+ */
+template <typename PointSource, typename PointTarget, typename Scalar = float>
+class IterativeClosestPointOMP
+    : public IterativeClosestPoint<PointSource, PointTarget, Scalar> {
+ public:
+  typedef
+      typename Registration<PointSource, PointTarget, Scalar>::PointCloudSource
+          PointCloudSource;
+  typedef typename PointCloudSource::Ptr PointCloudSourcePtr;
+  typedef typename PointCloudSource::ConstPtr PointCloudSourceConstPtr;
+
+  typedef
+      typename Registration<PointSource, PointTarget, Scalar>::PointCloudTarget
+          PointCloudTarget;
+  typedef typename PointCloudTarget::Ptr PointCloudTargetPtr;
+  typedef typename PointCloudTarget::ConstPtr PointCloudTargetConstPtr;
+
+  typedef PointIndices::Ptr PointIndicesPtr;
+  typedef PointIndices::ConstPtr PointIndicesConstPtr;
+
+  typedef boost::shared_ptr<
+      IterativeClosestPointOMP<PointSource, PointTarget, Scalar> >
+      Ptr;
+  typedef boost::shared_ptr<
+      const IterativeClosestPointOMP<PointSource, PointTarget, Scalar> >
+      ConstPtr;
+  /** \brief Empty constructor. */
+  IterativeClosestPointOMP()
+      : IterativeClosestPoint<PointSource, PointTarget, Scalar>() {
+    reg_name_ = "IterativeClosestPointOMP";
+    transformation_estimation_.reset(
+        new pcl::registration::TransformationEstimationSVD<
+            PointSource, PointTarget, Scalar>());
+    correspondence_estimation_.reset(
+        new pcl::registration::CorrespondenceEstimationOMP<
+            PointSource, PointTarget, Scalar>);
+    convergence_criteria_.reset(
+        new pcl::registration::DefaultConvergenceCriteria<Scalar>(
+            nr_iterations_, transformation_, *correspondences_));
   };
-}
- 
- 
+
+  /** \brief Empty destructor */
+  virtual ~IterativeClosestPointOMP() {}
+
+  double getFitnessScore(const PointCloudSourceConstPtr& cloud,
+                         double max_range = std::numeric_limits<double>::max());
+};
+}  // namespace pcl
+
 #include "impl/icp_omp.hpp"
 #endif  //#ifndef PCL_ICP_2D_OMP_H_
--- SlamUeben/include/impl/correspondence_estimation_omp.hpp
+++ SlamUeben/include/impl/correspondence_estimation_omp.hpp
@@ -1,169 +1,170 @@
 #ifndef PCL_REGISTRATION_IMPL_CORRESPONDENCE_ESTIMATION_OMP_H_
 #define PCL_REGISTRATION_IMPL_CORRESPONDENCE_ESTIMATION_OMP_H_
- 
-#include <pcl/common/io.h>
+
 #include <pcl/common/copy_point.h>
+#include <pcl/common/io.h>
 ///
-template <typename PointSource, typename PointTarget, typename Scalar> void
-pcl::registration::CorrespondenceEstimationOMP<PointSource, PointTarget, Scalar>::determineCorrespondences (
-    pcl::Correspondences &correspondences, double max_distance)
-{
-  if (!initCompute ())
-    return;
- 
+template <typename PointSource, typename PointTarget, typename Scalar>
+void pcl::registration::CorrespondenceEstimationOMP<
+    PointSource, PointTarget,
+    Scalar>::determineCorrespondences(pcl::Correspondences& correspondences,
+                                      double max_distance) {
+  if (!initCompute()) return;
+
   double max_dist_sqr = max_distance * max_distance;
   unsigned int nr_valid_correspondences = 0;
   pcl::Correspondences temp_correspondences;
-  temp_correspondences.resize (indices_->size ());
- 
+  temp_correspondences.resize(indices_->size());
+
   // Check if the template types are the same. If true, avoid a copy.
-  // Both point types MUST be registered using the POINT_CLOUD_REGISTER_POINT_STRUCT macro!
-  if (isSamePointType<PointSource, PointTarget> ()){
+  // Both point types MUST be registered using the
+  // POINT_CLOUD_REGISTER_POINT_STRUCT macro!
+  if (isSamePointType<PointSource, PointTarget>()) {
     // Iterate over the input set of source indices
-#pragma omp parallel for reduction(+:nr_valid_correspondences)
-      for (int i = 0;i< indices_->size(); ++i) {
-          int idx = (*indices_)[i];
-          std::vector<int> index(1);
-          std::vector<float> distance(1);
-          tree_->nearestKSearch (input_->points[idx], 1, index, distance);
-          if (distance[0] > max_dist_sqr) {
-              continue;
-          }
- 
-          temp_correspondences[i].index_query = idx;
-          temp_correspondences[i].index_match = index[0];
-          temp_correspondences[i].distance = distance[0];
-          nr_valid_correspondences++;
-    }
-     
-  }
-  else{
-    
-    // Iterate over the input set of source indices
-#pragma omp parallel for reduction(+:nr_valid_correspondences)
+#pragma omp parallel for reduction(+ : nr_valid_correspondences)
     for (int i = 0; i < indices_->size(); ++i) {
-        int idx = (*indices_)[i];
-      // Copy the source data to a target PointTarget format so we can search in the tree
-        PointTarget pt;
-        copyPoint (input_->points[idx], pt);
-        std::vector<int> index(1);
-        std::vector<float> distance(1);
-        tree_->nearestKSearch (pt, 1, index, distance);
-        if (distance[0] > max_dist_sqr) {
-            continue;
-        }
-         
-        temp_correspondences[i].index_query = idx;
-        temp_correspondences[i].index_match = index[0];
-        temp_correspondences[i].distance = distance[0];
-        nr_valid_correspondences++;
+      int idx = (*indices_)[i];
+      std::vector<int> index(1);
+      std::vector<float> distance(1);
+      tree_->nearestKSearch(input_->points[idx], 1, index, distance);
+      if (distance[0] > max_dist_sqr) {
+        continue;
+      }
+
+      temp_correspondences[i].index_query = idx;
+      temp_correspondences[i].index_match = index[0];
+      temp_correspondences[i].distance = distance[0];
+      nr_valid_correspondences++;
+    }
+
+  } else {
+    // Iterate over the input set of source indices
+#pragma omp parallel for reduction(+ : nr_valid_correspondences)
+    for (int i = 0; i < indices_->size(); ++i) {
+      int idx = (*indices_)[i];
+      // Copy the source data to a target PointTarget format so we can search in
+      // the tree
+      PointTarget pt;
+      copyPoint(input_->points[idx], pt);
+      std::vector<int> index(1);
+      std::vector<float> distance(1);
+      tree_->nearestKSearch(pt, 1, index, distance);
+      if (distance[0] > max_dist_sqr) {
+        continue;
+      }
+
+      temp_correspondences[i].index_query = idx;
+      temp_correspondences[i].index_match = index[0];
+      temp_correspondences[i].distance = distance[0];
+      nr_valid_correspondences++;
     }
   }
   correspondences.clear();
   correspondences.reserve(nr_valid_correspondences);
   for (const auto& corr : temp_correspondences) {
-      if (corr.index_match < 0) {
-          continue;
-      }
-      correspondences.push_back(corr);
+    if (corr.index_match < 0) {
+      continue;
+    }
+    correspondences.push_back(corr);
   }
-  deinitCompute ();
+  deinitCompute();
 }
- 
+
 ///
-template <typename PointSource, typename PointTarget, typename Scalar> void
-pcl::registration::CorrespondenceEstimationOMP<PointSource, PointTarget, Scalar>::determineReciprocalCorrespondences (
-    pcl::Correspondences &correspondences, double max_distance)
-{
-  if (!initCompute ())
-    return;
- 
+template <typename PointSource, typename PointTarget, typename Scalar>
+void pcl::registration::CorrespondenceEstimationOMP<PointSource, PointTarget,
+                                                    Scalar>::
+    determineReciprocalCorrespondences(pcl::Correspondences& correspondences,
+                                       double max_distance) {
+  if (!initCompute()) return;
+
   // setup tree for reciprocal search
   // Set the internal point representation of choice
-  if (!initComputeReciprocal())
-    return;
+  if (!initComputeReciprocal()) return;
   double max_dist_sqr = max_distance * max_distance;
- 
+
   pcl::Correspondences temp_correspondences;
   temp_correspondences.resize(indices_->size());
-  
+
   unsigned int nr_valid_correspondences = 0;
- 
+
   // Check if the template types are the same. If true, avoid a copy.
-  // Both point types MUST be registered using the POINT_CLOUD_REGISTER_POINT_STRUCT macro!
-  if (isSamePointType<PointSource, PointTarget> ()){
+  // Both point types MUST be registered using the
+  // POINT_CLOUD_REGISTER_POINT_STRUCT macro!
+  if (isSamePointType<PointSource, PointTarget>()) {
     // Iterate over the input set of source indices
-#pragma omp parallel for reduction(+:nr_valid_correspondences)
-    for (int i = 0; i < indices_->size (); ++i){
-        int idx = (*indices_)[i];
-        std::vector<int> index(1);
-        std::vector<float> distance(1);
-       
-        tree_->nearestKSearch (input_->points[idx], 1, index, distance);
-        if (distance[0] > max_dist_sqr) {
- 
-            continue;
-        }
- 
+#pragma omp parallel for reduction(+ : nr_valid_correspondences)
+    for (int i = 0; i < indices_->size(); ++i) {
+      int idx = (*indices_)[i];
+      std::vector<int> index(1);
+      std::vector<float> distance(1);
+
+      tree_->nearestKSearch(input_->points[idx], 1, index, distance);
+      if (distance[0] > max_dist_sqr) {
+        continue;
+      }
+
       int target_idx = index[0];
       std::vector<int> index_reciprocal(1);
       std::vector<float> distance_reciprocal(1);
-      tree_reciprocal_->nearestKSearch (target_->points[target_idx], 1, index_reciprocal, distance_reciprocal);
+      tree_reciprocal_->nearestKSearch(target_->points[target_idx], 1,
+                                       index_reciprocal, distance_reciprocal);
       if (distance_reciprocal[0] > max_dist_sqr || idx != index_reciprocal[0]) {
- 
-          continue;
+        continue;
       }
- 
+
+      temp_correspondences[i].index_query = idx;
+      temp_correspondences[i].index_match = index[0];
+      temp_correspondences[i].distance = distance[0];
+    }
+  } else {
+    // Iterate over the input set of source indices
+#pragma omp parallel for reduction(+ : nr_valid_correspondences)
+    for (int i = 0; i < indices_->size(); ++i) {
+      int idx = (*indices_)[i];
+      std::vector<int> index(1);
+      std::vector<float> distance(1);
+
+      // Copy the source data to a target PointTarget format so we can search in
+      // the tree
+      PointTarget pt_src;
+      copyPoint(input_->points[idx], pt_src);
+
+      tree_->nearestKSearch(pt_src, 1, index, distance);
+      if (distance[0] > max_dist_sqr) continue;
+
+      int target_idx = index[0];
+      std::vector<int> index_reciprocal(1);
+      std::vector<float> distance_reciprocal(1);
+      // Copy the target data to a target PointSource format so we can search in
+      // the tree_reciprocal
+      PointSource pt_tgt;
+      copyPoint(target_->points[target_idx], pt_tgt);
+
+      tree_reciprocal_->nearestKSearch(pt_tgt, 1, index_reciprocal,
+                                       distance_reciprocal);
+      if (distance_reciprocal[0] > max_dist_sqr || idx != index_reciprocal[0]) {
+        continue;
+      }
+
       temp_correspondences[i].index_query = idx;
       temp_correspondences[i].index_match = index[0];
       temp_correspondences[i].distance = distance[0];
     }
   }
-  else{
-    // Iterate over the input set of source indices
-#pragma omp parallel for reduction(+:nr_valid_correspondences)
-    for (int i = 0; i < indices_->size(); ++i) {
-        int idx = (*indices_)[i];
-        std::vector<int> index(1);
-        std::vector<float> distance(1);
- 
-        // Copy the source data to a target PointTarget format so we can search in the tree
-        PointTarget pt_src;
-        copyPoint (input_->points[idx], pt_src);
- 
-        tree_->nearestKSearch (pt_src, 1, index, distance);
-        if (distance[0] > max_dist_sqr)
-        continue;
- 
-        int target_idx = index[0];
-        std::vector<int> index_reciprocal(1);
-        std::vector<float> distance_reciprocal(1);
-        // Copy the target data to a target PointSource format so we can search in the tree_reciprocal
-        PointSource pt_tgt;
-        copyPoint (target_->points[target_idx], pt_tgt);
- 
-        tree_reciprocal_->nearestKSearch (pt_tgt, 1, index_reciprocal, distance_reciprocal);
-        if (distance_reciprocal[0] > max_dist_sqr || idx != index_reciprocal[0]) {
-            continue;
-        }
- 
-        temp_correspondences[i].index_query = idx;
-        temp_correspondences[i].index_match = index[0];
-        temp_correspondences[i].distance = distance[0];
-    }
-  }
   correspondences.clear();
   correspondences.reserve(nr_valid_correspondences);
   for (const auto& corr : temp_correspondences) {
-      if (corr.index_match < 0) {
-          continue;
-      }
-      correspondences.push_back(corr);
+    if (corr.index_match < 0) {
+      continue;
+    }
+    correspondences.push_back(corr);
   }
- 
-  deinitCompute ();
+
+  deinitCompute();
 }
- 
-//#define PCL_INSTANTIATE_CorrespondenceEstimation(T,U) template class PCL_EXPORTS pcl::registration::CorrespondenceEstimation<T,U>;
- 
+
+//#define PCL_INSTANTIATE_CorrespondenceEstimation(T,U) template class
+// PCL_EXPORTS pcl::registration::CorrespondenceEstimation<T,U>;
+
 #endif /* PCL_REGISTRATION_IMPL_CORRESPONDENCE_ESTIMATION_OMP_H_ */
