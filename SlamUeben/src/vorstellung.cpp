f5d231f7e9307d3625b3f3508efcd45742ab2014
zhicong.ding(at_office) <dingzhicong_auto@163.com>
2024-09-09 18:22:39 +0800

    add Handmade GN and LM

--- SlamUeben/src/vorstellung.cpp
+++ SlamUeben/src/vorstellung.cpp
@@ -2,7 +2,12 @@
 #include <vector>
 // #include <cstdlib> // 生成随机数的库，包含rand() srand()
 // #include <ctime> // 包含time()函数
-#include <algorithm> // sort()
+#include <algorithm>  // sort()
+#include <chrono>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Eigen>
+#include <opencv2/opencv.hpp>
 #include <random>
 #include <string>
 #include <typeinfo>
@@ -17,7 +22,7 @@ int Search(std::vector<double> &list, double goal) {
       return mid;
     }
     if (list[mid] < goal) {
-      left = mid + 1; // 小的+1大的-1,才能做到最后left > right
+      left = mid + 1;  // 小的+1大的-1,才能做到最后left > right
     } else {
       right = mid - 1;
     }
@@ -27,8 +32,8 @@ int Search(std::vector<double> &list, double goal) {
   std::cout << "list[right] = " << list[right] << std::endl;
   list.insert(
       list.begin() + left,
-      goal); // 最后是在left的地方插入，说明list[left]是比goal要大的，也就是最后达到的效果是
-             // list[right] < goal < list[left]!本来在右边的right已经跑到左边了
+      goal);  // 最后是在left的地方插入，说明list[left]是比goal要大的，也就是最后达到的效果是
+              // list[right] < goal < list[left]!本来在右边的right已经跑到左边了
   return -1;
 }
 
@@ -202,24 +207,119 @@ int main(int argc, char **argv) {
   double a = 1, b = 2, c = 3;
   std::vector<double> x_list;
   std::vector<double> y_list;
-  for(int i = -10; i < 10; i+=0.2) {
+  cv::RNG rng;
+  for (double i = -1; i < 1; i += 0.2) {
     x_list.emplace_back(i);
-    y_list.emplace_back(a*i*i + b * i + c + )
+    y_list.emplace_back(exp(a * i * i + b * i + c) + rng.gaussian(1.0));
   }
   {
-    //## 1-grad for optimization-> min ||fx||^2
-    std::cout << "## 1-grad for optimization-> min ||fx||^2" << std::endl;
+    //## 1-grad for optimization: min Fx = 1/2 ||fx||^2, Taylor on Fx
+    // min Fx -> dx = -Grad
     // dx = -J
+    std::cout << "## 1-grad for optimization-> min ||fx||^2" << std::endl;
   }
   {
-    //## 2-grad for optimization-> min ||fx||^2 (Newton)
-    std::cout << "/## 2-grad for optimization-> min ||fx||^2 (Newton)"
+    //## 2-grad for optimization: min Fx = 1/2 ||fx||^2, Taylor on Fx
+    // min Fx -> (Fx)' = 0 -> Fx + JxT dx + dxT Hx dx -> 对dx求导=0
+    // H dx = -J
+    std::cout << "## 2-grad for optimization-> min ||fx||^2 (Newton)"
               << std::endl;
-    // Z.B. newton solve y = a*x^2 + b*x + c
   }
   {
-    //## Gauss-Newton for optimization-> min ||fx||^2
+    //## Handmade Gauss-Newton for optimization: f(x+dx) = f(x) + J(x)dx, Fx = f(x+dx)T
+    // f(x+dx)
+    // Jx JxT dx = - Jx fx -> H dx = g
+    // Eigen::Vector3d test = Eigen::Vector3d::Zero();
+    // auto Mattest = test * test.transpose();
+    // std::cout << "test.typename = " << typeid(test).name() << std::endl;
+    // std::cout << "Mattest.cols = " << Mattest.cols()
+    //           << ", Mattest.rows = " << Mattest.rows() << std::endl;
     std::cout << "## Gauss-Newton for optimization-> min ||fx||^2" << std::endl;
+    std::cout << "### self wrote" << std::endl;
+    const int size = x_list.size();
+    Eigen::Vector3d J;
+    Eigen::MatrixXd H(size * 3, 3);
+    Eigen::MatrixXd g(size * 3, 1);
+    double fxi;
+    Eigen::Vector3d x_est(1.0, 1.0, 1.0);
+    Eigen::Vector3d dx;
+    double exp_tmp;
+    int maxIters = 100;
+    bool satisfied = false;
+    int iters = 0;
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+    for (; iters < maxIters; iters++) {
+      for (int i = 0; i < size; i++) {
+        exp_tmp = exp(x_est[0] * x_list[i] * x_list[i] + x_est[1] * x_list[i] +
+                      x_est[2]);
+        J(2) = -exp_tmp;
+        J(1) = J(2) * x_list[i];
+        J(0) = J(1) * x_list[i];
+        fxi = y_list[i] - exp_tmp;
+        H.block<3, 3>(3 * i, 0) = J * J.transpose();
+        g.block<3, 1>(3 * i, 0) = -J * fxi;
+      }
+      dx = H.colPivHouseholderQr().solve(g);
+      std::cout << "dx = " << dx << std::endl;
+      if (isnan(dx[0])) {
+        std::cout << "result is nan!" << std::endl;
+        break;
+      }
+      x_est += dx;
+    //   if (dx.norm() < 1e-10) {
+    //     satisfied = true;
+    //     break;
+    //   }
+    }
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> time_mine =
+        std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    std::cout << "It takes " << time_mine.count() << "sec, iters = " << iters
+              << ", x_est = " << x_est.transpose() << std::endl;
+
+    std::cout << "### from Gao" << std::endl;
+    // Here, i set Hx = g as so: H(datasize * 3, 3), x(3, 1), g(datasize*3, 1)
+    // Yet Gao set Hx = g as: H(3, 3), x(3, 1), g(3, 1), which means he adds up
+    // all data into one equation for each iteration.
+
+    Eigen::Vector3d J_gao;
+    Eigen::MatrixXd H_gao(3, 3);
+    Eigen::MatrixXd g_gao(3, 1);
+    x_est << 1.0, 1.0, 1.0;
+    iters = 0;
+    t1 = std::chrono::steady_clock::now();
+    for (; iters < maxIters; iters++) {
+      H_gao.setZero();
+      g_gao.setZero();
+      for (int i = 0; i < size; i++) {
+        exp_tmp = exp(x_est[0] * x_list[i] * x_list[i] + x_est[1] * x_list[i] +
+                      x_est[2]);
+        J_gao(2) = -exp_tmp;
+        J_gao(1) = -exp_tmp * x_list[i];
+        J_gao(0) = -exp_tmp * x_list[i] * x_list[i];
+        H_gao += J_gao * J_gao.transpose();
+        fxi = y_list[i] - exp_tmp;
+        g_gao += -J_gao * fxi;
+      }
+      dx = H_gao.colPivHouseholderQr().solve(g_gao);
+      if (isnan(dx[0])) {
+        std::cout << "result is nan!" << std::endl;
+        break;
+      }
+      x_est += dx;
+    //   if (dx.norm() < 1e-10) {
+    //     satisfied = true;
+    //     break;
+    //   }
+    }
+    t2 = std::chrono::steady_clock::now();
+    time_mine =
+        std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    std::cout << "It takes " << time_mine.count() << "sec, iters = " << iters
+              << ", x_est = " << x_est.transpose() << std::endl;
+  }
+  {
+    //## Handmade LM
   }
   return 0;
 }
